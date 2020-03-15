f2a102faec01802b87c8fd4dd12a811c9bb073f6
sjmars <sjmarsnc@gmail.com>
2020-03-14 23:22:01 -0400

    fix port to work on heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("App running on port 3000!");
 });
 
